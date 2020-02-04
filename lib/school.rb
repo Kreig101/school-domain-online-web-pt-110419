ba32f8e73951c51852005744d1afe1299da09c9b
Craig Storer <craigstorer@gmail.com>
2020-02-04 14:44:21 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+require 'pry'
+class School
+
+ attr_accessor :roster, :name
+  
+  def initialize (name)
+    @name = name
+    @roster = {}
+  end
+
+  
+
+  def add_student(name,grade)
+  
+    add_student[grade] = [name]
+    
+  end
+end
+
+
+
